10dbca366573ca6b982784fb074c7a0f42dd7831
kingkor399 <farhanrahmanyou@gmail.com>
2022-09-29 21:31:59 +0600

    nine commit

--- src/comoponent/Q&A/Question.js
+++ src/comoponent/Q&A/Question.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 const Question = () => {
     return (
-        <footer>
-            <h3>Hello</h3>
+        <footer className='question-text'>
+            <h2>Question & Answer</h2>
+            <h4><u>Question:</u> How does react work</h4>
+            <h4><u>Answer:</u> React is a JavaScript library (not a framework) that creates user interfaces (UIs) in a predictable and efficient way using declarative code. You can use it to help build single page applications and mobile apps, or to build complex apps if you utilise it with other libraries.</h4>
+            <h4><u>Question:</u> Props and State Different</h4>
+            <h4><u>Answer:</u> Props are used to pass data from one component to another. The state is a local data storage that is local to the component only and cannot be passed to other components</h4>
+            <h4><u>Question:</u> How does react work</h4>
+            <h4><u>Answer:</u></h4>
         </footer>
     );
 };
