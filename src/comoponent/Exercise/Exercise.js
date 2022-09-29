fe850bac1e57456c0254aa0509705f311871dc61
kingkor399 <farhanrahmanyou@gmail.com>
2022-09-29 11:27:17 +0600

    third commit

--- src/comoponent/Exercise/Exercise.js
+++ src/comoponent/Exercise/Exercise.js
@@ -5,10 +5,17 @@ const Exercise = (props) => {
     return (
         <div className='exercise'>
             <img src={img}></img>
+            <div>
             <p className='name-text'>Name: {name}</p>
-            <p></p>
+            <p>Times a week I do exercise in the evening, so I can stay strong and ... daily routine is a little boring – 
+                but on weekends and holidays I love to fly...
+            </p>
             <p>Age: {age}</p>
             <p>Time required: {Time}</p>
+            </div>
+            <button className='btn-cart'>
+                <h3>Add To List</h3>
+            </button>
         </div>
     );
 };
